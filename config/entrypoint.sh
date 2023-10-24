16fafbf0717f677e79b7653ba8d3b9065c31696e
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2023-10-24 11:12:21 +0200

    Add namespace and image in kubernetes config

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -6,4 +6,6 @@ gitlab-runner register \
 	--url $GITLAB_URL \
 	--token $GITLAB_REGISTRATION_TOKEN \
 	--executor $EXECUTOR \
+	--kubernetes-image $IMAGE \
+	--kubernetes-namespace $NAMESPACE \
 	&& gitlab-runner run;
